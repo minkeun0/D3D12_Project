32f42c93d94688987a964e506b2017ec80050c17
minkeun <minkeun123@gmail.com>
2024-09-17 21:50:49 +0900

    begin

---
+++ Framework.h
@@ -0,0 +1,11 @@
+#pragma once
+class Framework
+{
+public:
+	Framework() {};
+
+	virtual void OnInit();
+
+private:
+};
+
---
+++ Main.cpp
@@ -0,0 +1,12 @@
+﻿// Project.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "Scene.h"
+
+_Use_decl_annotations_
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    Scene sample(1280, 720, L"MyGame");
+    return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
